4e1e4e3afe1fa522f294515b3e85a55102577616
Infinity-INF <131372039+Infinity-INF@users.noreply.github.com>
2023-08-07 22:21:24 +0800

    Add files via upload
    upload V1.0

---
+++ infer.py
@@ -0,0 +1,26 @@
+import librosa
+import torch
+import whisper_ph_asr
+import genshin
+
+devices=torch.cuda.is_available()
+if devices==True:
+    print("Use CUDA")
+    device=torch.device('cuda')
+    asr = whisper_ph_asr.PhonemeAsr().cuda()
+else:
+    print("Use CPU")
+    torch.device('cpu')
+    asr = whisper_ph_asr.PhonemeAsr().cpu()
+
+print("input your sounds directory:")
+sounddir=input()
+
+pth = "phasr.pth"
+ckpt = torch.load(pth)
+asr.load_state_dict(ckpt)
+
+wav16k, _ = librosa.load(sounddir, sr=16000)
+phonemes, durations = whisper_ph_asr.get_asr_result(asr, wav16k)
+
+print(phonemes, durations)
