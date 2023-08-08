3947ca50188e7b94a977db14c5975c3c4a1e848b
C7H4NNaO3S <2769809079@qq.com>
2023-08-08 11:22:02 +0800

    add web infer function

--- infer.py
+++ infer.py
@@ -1,26 +1,34 @@
 import librosa
 import torch
 import whisper_ph_asr
-import genshin
+#import genshin
+def infer(model,inferdevice,file):
+    if inferdevice=="自动":
+        devices=torch.cuda.is_available()
+        if devices==True:
+            print("Use CUDA")
+            device=torch.device('cuda')
+            asr = whisper_ph_asr.PhonemeAsr().cuda()
+        else:
+            print("Use CPU")
+            torch.device('cpu')
+            asr = whisper_ph_asr.PhonemeAsr().cpu()
+    else:
+        devices = torch.cuda.is_available()
+        print("Use CPU")
+        torch.device('cpu')
+        asr = whisper_ph_asr.PhonemeAsr().cpu()
 
-devices=torch.cuda.is_available()
-if devices==True:
-    print("Use CUDA")
-    device=torch.device('cuda')
-    asr = whisper_ph_asr.PhonemeAsr().cuda()
-else:
-    print("Use CPU")
-    torch.device('cpu')
-    asr = whisper_ph_asr.PhonemeAsr().cpu()
+    sounddir=file
 
-print("input your sounds directory:")
-sounddir=input()
+    pth = model
+    ckpt = torch.load(pth)
+    asr.load_state_dict(ckpt)
 
-pth = "phasr.pth"
-ckpt = torch.load(pth)
-asr.load_state_dict(ckpt)
+    wav16k, _ = librosa.load(sounddir, sr=16000)
+    phonemes, durations = whisper_ph_asr.get_asr_result(asr, wav16k)
 
-wav16k, _ = librosa.load(sounddir, sr=16000)
-phonemes, durations = whisper_ph_asr.get_asr_result(asr, wav16k)
-
-print(phonemes, durations)
+    print(phonemes, durations)
+    yield phonemes,durations
+if __name__ =="__main__":
+    infer()
