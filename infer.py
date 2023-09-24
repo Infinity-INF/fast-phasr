f1f473262f8869bc4d444b4e642b60dda5c8fda5
SuSWhW <anjiurine@outlook.com>
2023-09-24 21:21:48 +0800

    添加字典替换规则

--- infer.py
+++ infer.py
@@ -4,8 +4,17 @@ import torch
 import librosa
 import sys
 from tqdm import tqdm
+import yaml
 
 import whisper_ph_asr
+
+with open('phoneme_combinations_to_pinyin_dict.yaml', 'r', encoding='utf-8') as dict_file:
+    yaml_dict = yaml.safe_load(dict_file)
+    phoneme_combinations_to_pinyin_dict = {}
+    for entry in yaml_dict['entries']:
+        grapheme = entry['grapheme']
+        phonemes = ''.join(entry['phonemes'])
+        phoneme_combinations_to_pinyin_dict[grapheme] = phonemes
 
 devices = torch.cuda.is_available()
 if devices:
@@ -39,10 +48,27 @@ for wav_file in tqdm(wav_file_list, desc="Processing", ncols=100):
     wav16k, _ = librosa.load(wav_file, sr=16000)
     phonemes, durations = whisper_ph_asr.get_asr_result(asr, wav16k)
 
-    htk_labels = [label for label in phonemes if label not in ["SP", "AP"]]
+    clean_phonemes = [p for p in phonemes if p not in {"SP", "AP"}]
+    pinyin_sequence = ""
+    i = 0
+    while i < len(clean_phonemes):
+        combined_pinyin = None
+        for j in range(len(clean_phonemes), i, -1):
+            phoneme_combination = ''.join(clean_phonemes[i:j])
+            if phoneme_combination in phoneme_combinations_to_pinyin_dict:
+                combined_pinyin = phoneme_combinations_to_pinyin_dict[phoneme_combination]
+                break
+        if combined_pinyin:
+            pinyin_sequence += combined_pinyin + " "
+            i = j
+        else:
+            pinyin_sequence += phoneme_combinations_to_pinyin_dict.get(clean_phonemes[i], clean_phonemes[i]) + " "
+            i += 1
 
     output_filename = os.path.splitext(os.path.basename(wav_file))[0] + ".lab"
     output_path = os.path.join(os.path.dirname(wav_file), output_filename)
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.write(pinyin_sequence.rstrip())
 
     sys.stdout.write("\rProcessing: {:3}%|".format(int((wav_file_list.index(wav_file) + 1) / len(wav_file_list) * 100)))
     sys.stdout.flush()
