76c7061f935fb36a45bb830e1b31dba162b2de16
SuSWhW <anjiurine@outlook.com>
2023-09-24 19:58:09 +0800

    refactor: :recycle: refactor

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # fast-phasr
-Phonemes and durations labeling based on whisper small
+Phonemes labeling based on whisper small
+
+## Use
+
+### Install dependencies
+
+```
+# gpu
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+# cpu
+pip3 install torch torchvision torchaudio
+
+pip install -r requirement.txt
+```
+
+### Inference
+
+```
+python infer.py [yor wav directory]
+```
+
+## Requirement
+
+```
+ffmpeg==1.4
+librosa==0.10.0.post2
+numpy==1.24.4
+gradio
+torch
+```
--- infer.py
+++ infer.py
@@ -1,25 +1,50 @@
-import librosa
+import argparse
+import os
 import torch
+import librosa
+import sys
+from tqdm import tqdm
+
 import whisper_ph_asr
 
-devices=torch.cuda.is_available()
-if devices==True:
+devices = torch.cuda.is_available()
+if devices:
     print("Use CUDA")
-    device=torch.device('cuda')
+    device = torch.device('cuda')
     asr = whisper_ph_asr.PhonemeAsr().cuda()
 else:
     print("Use CPU")
-    torch.device('cpu')
+    device = torch.device('cpu')
     asr = whisper_ph_asr.PhonemeAsr().cpu()
 
-print("input your sounds directory:")
-sounddir=input()
+parser = argparse.ArgumentParser(description="Batch inference for audio files")
+parser.add_argument("input_dir", type=str, help="Input directory containing WAV files")
+args = parser.parse_args()
+
+def get_wav_file_list(input_dir):
+    wav_file_list = []
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".wav"):
+            wav_file_list.append(os.path.join(input_dir, filename))
+    return wav_file_list
+
+sounddir = args.input_dir
+wav_file_list = get_wav_file_list(sounddir)
 
 pth = "phasr.pth"
 ckpt = torch.load(pth)
 asr.load_state_dict(ckpt)
 
-wav16k, _ = librosa.load(sounddir, sr=16000)
-phonemes, durations = whisper_ph_asr.get_asr_result(asr, wav16k)
+for wav_file in tqdm(wav_file_list, desc="Processing", ncols=100):
+    wav16k, _ = librosa.load(wav_file, sr=16000)
+    phonemes, durations = whisper_ph_asr.get_asr_result(asr, wav16k)
 
-print(phonemes, durations)
+    htk_labels = [label for label in phonemes if label not in ["SP", "AP"]]
+
+    output_filename = os.path.splitext(os.path.basename(wav_file))[0] + ".lab"
+    output_path = os.path.join(os.path.dirname(wav_file), output_filename)
+
+    sys.stdout.write("\rProcessing: {:3}%|".format(int((wav_file_list.index(wav_file) + 1) / len(wav_file_list) * 100)))
+    sys.stdout.flush()
+
+print("\nInference completed")
